143eb8b973890f685e3be68193bc39aad13901d4
Conor Sayres <csayres@uw.edu>
2021-07-23 08:14:09 -0700

    out and back, and safe trajectories

--- fvcLoop.py
+++ fvcLoop.py
@@ -1,4 +1,5 @@
 import asyncio
+import numpy
 
 from jaeger import FPS, log
 from kaiju.robotGrid import RobotGridCalib
@@ -16,9 +17,39 @@ CONTINUOUS = True
 UNWINDONLY = False
 TAKE_IMGS = False
 LED_VALUE = 52
+alphaHome = 0
+betaHome = 180
+seed = None
+escapeDeg = 20 # 20 degrees of motion to escape
 
-rg = RobotGridCalib(angStep, collisionBuffer, epsilon)
-fps = FPS()
+if seed is None:
+    seed = numpy.random.randint(0, 30000)
+
+print("seed", seed)
+
+rg = RobotGridCalib(angStep, collisionBuffer, epsilon, seed)
+
+
+async def updateCurrentPos(fps):
+    for r in rg.robotDict.values():
+        await fps.positioners[r.id].update_position()
+        alpha, beta = fps.positioners[r.id].position
+        r.setAlphaBeta(alpha, beta)
+        r.setDestinationAlphaBeta(alphaHome, betaHome)
+
+    for r in rg.robotDict.values():
+        if rg.isCollided(r.id):
+            print("robot ", r.id, " is collided")
+
+
+async def escape(fps):
+    print("escape")
+    await updateCurrentPos(fps)
+    rg.pathGenEscape(escapeDeg)
+    forwardPath, reversePath = rg.getPathPair()
+    print("didFail", rg.didFail)
+    print("smooth collisions", rg.smoothCollisions)
+    await fps.send_trajectory(forwardPath)
 
 
 async def unwindGrid(fps):
@@ -30,25 +61,56 @@ async def unwindGrid(fps):
     """
     # overwrite the positions to the positions that the robots
     # are reporting
-    for r in rg.robotDict.values():
-        await fps.positioners[r.id].update_position()
-        alpha, beta = fps.positioners[r.id].position
-        r.setAlphaBeta(alpha, beta)
-
-    for r in rg.robotDict.values():
-        if rg.isCollided(r.id):
-            print("robot ", r.id, " is collided")
+    print("unwind grid")
+    await updateCurrentPos(fps)
     forwardPath, reversePath = rg.getPathPair()
     print("didFail", rg.didFail)
     print("smooth collisions", rg.smoothCollisions)
-    print(forwardPath)
-    print(reversePath)
+    # print(forwardPath)
+    # print(reversePath)
 
-    # await fps.send_trajectory(reversePath)
+    await fps.send_trajectory(reversePath)
+
+
+async def outAndBackSafe(fps):
+    print("out and back safe")
+    forwardPath, reversePath = rg.getRandomPathPair(
+        alphaHome=alphaHome, betaHome=betaHome, betaSafe=True
+    )
+    print("didFail", rg.didFail)
+    print("smooth collisions", rg.smoothCollisions)
+
+    if not rg.didFail and rg.smoothCollisions == 0:
+        await fps.send_trajectory(forwardPath)
+        await fps.send_trajectory(reversePath)
+    else:
+        print("not sending path")
+
+
+async def outAndBackUnsafe(fps):
+    print("out and back UNsafe")
+    forwardPath, reversePath = rg.getRandomPathPair(
+        alphaHome=alphaHome, betaHome=betaHome, betaSafe=False
+    )
+
+    print("didFail", rg.didFail)
+    print("smooth collisions", rg.smoothCollisions)
+    if not rg.didFail and rg.smoothCollisions == 0:
+        await fps.send_trajectory(forwardPath)
+        await fps.send_trajectory(reversePath)
+    else:
+        print("not sending path")
 
 async def main():
+    fps = FPS()
     await fps.initialise()
     await unwindGrid(fps)
+    await outAndBackSafe(fps)
+    await outAndBackUnsafe(fps)
+    # for i in range(5):
+    #     await escape(fps)
+
+    await fps.shutdown()
 
 
 if __name__ == "__main__":
