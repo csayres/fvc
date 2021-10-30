0b04e4942056aaec20c5004fa4680dbbf0b4fb93
Conor Sayres <csayres@uw.edu>
2021-10-30 10:51:22 -0400

    start process imgs

--- imgsnap.py
+++ imgsnap.py
@@ -20,7 +20,7 @@ async def main(exptime, led1, led2):
         await device.write(on_value)
 
     filename = await exposeFVC(exptime)
-    await writeProcFITS(filename, fps, rg, seed, expectedTargCoords, doProcess=False)
+    await writeProcFITS(filename, fps, rg, seed, expectedTargCoords, doProcess=True)
 
 if __name__ == "__main__":
 
